724af3bf6f9a5eab13938c8303b74afbfca70b33
Jonas Ornfelt <jonasornfelt@outlook.com>
2023-07-27 06:59:50 +0200

    Added another script for server

---
+++ Scripts/wcell.ps1
@@ -0,0 +1,9 @@
+$path = "C:\Users\jonas\Code2\C#\wcell_\Run\Debug"
+
+cd $path
+
+#echo "$path\WCell.RealmServerConsole.exe"
+#Invoke-Expression "$path\WCell.RealmServerConsole.exe"
+
+#echo "python $path\overwrite.py; $path\WCell.RealmServerConsole.exe"
+#Invoke-Expression "python $path\overwrite.py; $path\WCell.RealmServerConsole.exe"
