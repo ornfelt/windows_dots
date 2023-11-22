f1dadd3f6bb6039051f198b3b8fbb720f71f2e3f
Jonas Ornfelt <jonasornfelt@outlook.com>
2023-11-22 11:21:18 +0100

    Update powershell script

--- my_scripts/playermap.ps1
+++ my_scripts/playermap.ps1
@@ -3,8 +3,8 @@ $server = $args[0]
 if ($server -eq "tcore")
 {
 	echo "Launching tcore playermap: php -S localhost:8000"
-	if (Test-Path "C:\Users\jonas\Code2\tcore_map\playermap") {
-		$path = "C:\Users\jonas\Code2\tcore_map\playermap"
+	if (Test-Path "C:\Users\jonas\Code2\Python\wander_nodes_util\tcore_map\playermap") {
+		$path = "C:\Users\jonas\Code2\Python\wander_nodes_util\tcore_map\playermap"
 	} elseif (Test-Path "D:\My files\svea_laptop\code_hdd\repos\Code2\Python\wander_nodes_util\tcore_map\playermap") {
 		$path = "D:\My files\svea_laptop\code_hdd\repos\Code2\Python\wander_nodes_util\tcore_map\playermap"
 	} elseif (Test-Path "x") {
@@ -16,8 +16,8 @@ if ($server -eq "tcore")
 else
 {
 	echo "Launching acore playermap: php -S localhost:8000"
-		if (Test-Path "C:\Users\jonas\Code2\\acore_map\playermap") {
-		$path = "C:\Users\jonas\Code2\\acore_map\playermap"
+		if (Test-Path "C:\Users\jonas\Code2\Python\wander_nodes_util\acore_map\playermap") {
+		$path = "C:\Users\jonas\Code2\Python\wander_nodes_util\acore_map\playermap"
 	} elseif (Test-Path "D:\My files\svea_laptop\code_hdd\repos\Code2\Python\wander_nodes_util\acore_map\playermap") {
 		$path = "D:\My files\svea_laptop\code_hdd\repos\Code2\Python\wander_nodes_util\acore_map\playermap"
 	} elseif (Test-Path "x") {
