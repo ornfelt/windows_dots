e2a1943f6cc2b35a7ed0172d3e63369aa44ec0e7
Jonas Örnfelt <jonasornfelt@outlook.com>
2024-08-01 15:57:54 +0200

    update ps script

--- my_scripts/wcell.ps1
+++ my_scripts/wcell.ps1
@@ -1,15 +1,22 @@
-$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2\C#\wcell\Run\Debug"
+$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2\C#\WCell\Run\Debug"
 
 if (Test-Path $basePath) {
     $path = $basePath
-} elseif (Test-Path "C:\Users\jonas\Code2\C#\wcell_\Run\Debug") {
-	$path = "C:\Users\jonas\Code2\C#\wcell_\Run\Debug"
-} elseif (Test-Path "D:\My files\svea_laptop\code_hdd\WCell_\Run\Debug") {
-	$path = "D:\My files\svea_laptop\code_hdd\WCell_\Run\Debug"
-} elseif (Test-Path "x") {
-	$path = "x"
 } else {
-	$path = "~/"
+    $basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2\C#\wcell_\Run\Debug"
+    if (Test-Path $basePath) {
+        $path = $basePath
+    } elseif (Test-Path "C:\Users\jonas\Code2\C#\wcell_\Run\Debug") {
+        $path = "C:\Users\jonas\Code2\C#\wcell_\Run\Debug"
+    } elseif (Test-Path "D:\My files\svea_laptop\code_hdd\WCell_\Run\Debug") {
+        $path = "D:\My files\svea_laptop\code_hdd\WCell_\Run\Debug"
+    } elseif (Test-Path "x") {
+        $path = "x"
+    } else {
+        #$path = "~/"
+        Write-Host "Couldn't find wcell build path. Is it compiled?"
+            exit 1
+    }
 }
 
 cd $path
@@ -19,3 +26,4 @@ echo "$path\WCell.RealmServerConsole.exe"
 
 #echo "python $path\overwrite.py; $path\WCell.RealmServerConsole.exe"
 #Invoke-Expression "python $path\overwrite.py; $path\WCell.RealmServerConsole.exe"
+
