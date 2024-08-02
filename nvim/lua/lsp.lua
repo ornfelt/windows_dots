dd66bc3dbcef7aa83afe6c32e1f0f28c4f6d6756
ornfelt <jonasornfelt@outlook.com>
2024-08-02 19:07:21 +0200

    update yamlls

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -79,8 +79,18 @@ setup_lsp_if_available('rust_analyzer', lsp_attach_config, 'rust-analyzer')
 setup_lsp_if_available('fsautocomplete', lsp_attach_config)
 setup_lsp_if_available('jdtls', lsp_attach_config)
 setup_lsp_if_available('bashls', lsp_attach_config, 'bash-language-server')
-setup_lsp_if_available('yamlls', lsp_attach_config, 'yaml-language-server')
 setup_lsp_if_available('sqls', lsp_attach_config)
+
+lspconfig.yamlls.setup {
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+        -- other schemas...
+      },
+    },
+  }
+}
 
 local omnisharp_path = os.getenv('OMNISHARP_PATH')
 if omnisharp_path then
