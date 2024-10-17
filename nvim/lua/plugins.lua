128c267a375ab44fb1faddd8a79354303b44c35a
ornfelt <jonasornfelt@outlook.com>
2024-10-17 17:50:26 +0200

    hmm

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -37,10 +37,10 @@ return require('packer').startup(function()
   use 'hrsh7th/nvim-cmp'
   use("gruvbox-community/gruvbox")
 
-  -- use {
-      -- 'nvim-treesitter/nvim-treesitter',
-      -- build = ':TSUpdate'
-  -- }
+  use {
+      'nvim-treesitter/nvim-treesitter',
+      build = ':TSUpdate'
+  }
 
   use {
     'nvim-telescope/telescope.nvim',
@@ -108,6 +108,24 @@ return require('packer').startup(function()
           },
       }
   }
+
+  -- use({
+      -- 'MeanderingProgrammer/render-markdown.nvim',
+      -- after = { 'nvim-treesitter' },
+      -- -- requires = { 'echasnovski/mini.nvim', opt = true }, -- if you use the mini.nvim suite
+      -- -- requires = { 'echasnovski/mini.icons', opt = true }, -- if you use standalone mini plugins
+      -- -- requires = { 'nvim-tree/nvim-web-devicons', opt = true }, -- if you prefer nvim-web-devicons
+      -- config = function()
+          -- require('render-markdown').setup({})
+      -- end,
+  -- })
+  -- use ({
+      -- "OXY2DEV/markview.nvim",
+      -- dependencies = {
+          -- "nvim-treesitter/nvim-treesitter",
+          -- "nvim-tree/nvim-web-devicons"
+      -- }
+  -- })
 
   -- use 'alexghergh/nvim-tmux-navigation'
   -- use 'mhinz/vim-startify'
