0316d20d667f35ae95a6d29736bcdc861d4a6093
ornfelt <jonasornfelt@outlook.com>
2024-10-30 15:29:26 +0100

    shada clean script

--- my_scripts/clean_shada.ps1
+++ my_scripts/clean_shada.ps1
@@ -5,3 +5,12 @@ Remove-Item "$shadaPath\*" -Force
 # Or:
 # Remove-Item "$env:LOCALAPPDATA\nvim-data\shada\*" -Force
 
+
+# Notes
+# nvim shada issue:
+# rm ~/.local/share/nvim/shada/*
+# rm  ~/.local/state/nvim/shada/* fixed the issue.
+# Windows: %LOCALAPPDATA%\nvim-data
+# vim.opt.shadafile = "NONE"
+# probably shouldnt disable it though...
+
