3b953ff7aabfa92e54953e238e6f196c24ac7dc3
ornfelt <jonasornfelt@outlook.com>
2024-11-12 12:40:39 +0100

    update script

--- my_scripts/update_nvim_from_linux.ps1
+++ my_scripts/update_nvim_from_linux.ps1
@@ -1,5 +1,12 @@
 $codeRootDir = $env:code_root_dir
 
+# Prompt for confirmation before proceeding
+$confirmation = Read-Host "Do you want to proceed? (y/n)"
+if ($confirmation -notmatch "^(?i)y(?:es)?$") {
+    Write-Output "Operation canceled by the user."
+    exit 0
+}
+
 # Define source and target dirs
 $dotfilesDir = Join-Path -Path $codeRootDir -ChildPath "Code2/General/dotfiles/.config"
 $nvimSourceDir = Join-Path -Path $dotfilesDir -ChildPath "nvim"
