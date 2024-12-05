5e6aadb930b72e0f18471e5853655318eace5d57
ornfelt <jonasornfelt@outlook.com>
2024-12-05 17:46:05 +0100

    restore tab keybind for nvim

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -48,7 +48,56 @@ map('i', '<C-v>', '<Esc>"+p')
 map('i', '<S-Insert>', '<Esc><MiddleMouse>A')
 map('n', '<S-Insert>', '<MiddleMouse>')
 
-map('n', '<M-q>', ':q<CR>') -- Quit
+--map('n', '<M-q>', ':q<CR>') -- Quit
+-- Close and restore tab
+local last_closed_tab = nil
+
+local function save_and_close_tab()
+  local tabpage = vim.api.nvim_get_current_tabpage()
+  local windows = vim.api.nvim_tabpage_list_wins(tabpage)
+  local buffers = {}
+
+  for _, win in ipairs(windows) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    table.insert(buffers, {
+      name = vim.api.nvim_buf_get_name(buf),
+      position = vim.api.nvim_win_get_cursor(win),
+    })
+  end
+
+  last_closed_tab = buffers
+
+  vim.cmd("tabclose")
+end
+
+local function restore_tab()
+  if not last_closed_tab or #last_closed_tab == 0 then
+    print("No closed tab to restore.")
+    return
+  end
+
+  vim.cmd("tabnew")
+
+  local current_tab_index = vim.fn.tabpagenr()
+  local total_tabs = vim.fn.tabpagenr("$")
+
+  if current_tab_index < total_tabs then
+    vim.cmd("tabmove -1")
+  end
+
+  for _, buf_data in ipairs(last_closed_tab) do
+    if buf_data.name ~= "" then
+      vim.cmd("edit " .. buf_data.name)
+      vim.api.nvim_win_set_cursor(0, buf_data.position)
+    end
+  end
+
+  last_closed_tab = nil
+end
+
+vim.keymap.set("n", "<M-q>", save_and_close_tab, { noremap = true, silent = true })
+vim.keymap.set("n", "<M-S-T>", restore_tab, { noremap = true, silent = true })
+
 map('n', '<M-z>', ':noh<CR>')
 map('n', 'Y', 'y$') -- Yank till end of line
 
