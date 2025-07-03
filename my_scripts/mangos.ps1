b9a9f01d216373718c5370b2976c3272f589c10c
Jonas Ornfelt <jonasornfelt@outlook.com>
2025-07-03 09:02:22 +0200

    update mangos script

--- my_scripts/mangos.ps1
+++ my_scripts/mangos.ps1
@@ -3,57 +3,51 @@ $server = $args[0]
 # MangosZero
 if ($server -ieq "0" -or $server -ieq "z") {
 	echo "MangosZero chosen..."
-	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/mangoszero/server/build/src/mangosd/RelWithDebInfo"
-	$altBasePathasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/server/build/src/mangosd/RelWithDebInfo"
+	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/server/build/src/mangosd/RelWithDebInfo"
+	$altBasePathasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/mangoszero/server/build/src/mangosd/RelWithDebInfo"
 
 	if (Test-Path $basePath) {
 		$path = $basePath
     } elseif (Test-Path $altBasePath) {
 		$path = $altBasePath
-	} elseif (Test-Path "D:\My files\svea_laptop\mangoszero\server\build\src\mangosd\RelWithDebInfo") {
-		$path = "D:\My files\svea_laptop\mangoszero\server\build\src\mangosd\RelWithDebInfo"
 	} else {
 		$path = "~/mangoszero/bin"
 	}
 # Cmangos
 } elseif ($server -ieq "c") {
 	echo "Cmangos chosen..."
-	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/cmangos/mangos-classic/build/bin/x64_RelWithDebInfo"
-	$altBasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/mangos-classic/build/bin/x64_RelWithDebInfo"
+	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/mangos-classic/build/bin/x64_RelWithDebInfo"
+	$altBasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/cmangos/mangos-classic/build/bin/x64_RelWithDebInfo"
 
 	if (Test-Path $basePath) {
 		$path = $basePath
     } elseif (Test-Path $altBasePath) {
 		$path = $altBasePath
-	} elseif (Test-Path "D:\My files\svea_laptop\cmangos\mangos-classic\build\bin\x64_RelWithDebInfo") {
-		$path = "D:\My files\svea_laptop\cmangos\mangos-classic\build\bin\x64_RelWithDebInfo"
 	} else {
 		$path = "~/cmangos/run/bin"
 	}
 
 } elseif ($server -ieq "tbc") {
 	echo "Cmangos tbc chosen..."
-	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/cmangos/mangos-tbc/build/bin/x64_RelWithDebInfo"
-	$altBasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/mangos-tbc/build/bin/x64_RelWithDebInfo"
+	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/mangos-tbc/build/bin/x64_RelWithDebInfo"
 
 	if (Test-Path $basePath) {
 		$path = $basePath
-    } elseif (Test-Path $altBasePath) {
-		$path = $altBasePath
+    } else {
+        Write-Host "Base path does not exist: $basePath"
+        exit 1
     }
 
 # Default to Vmangos
 } else {
 	echo "Vmangos chosen..."
-	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/vmangos/core/bin/RelWithDebInfo"
-	$altBasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/core/bin/RelWithDebInfo"
+	$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/core/bin/RelWithDebInfo"
+	$altBasePath = Join-Path -Path $env:code_root_dir -ChildPath "Code2/C++/vmangos/core/bin/RelWithDebInfo"
 	
 	if (Test-Path $basePath) {
 		$path = $basePath
     } elseif (Test-Path $altBasePath) {
 		$path = $altBasePath
-	} elseif (Test-Path "D:\My files\svea_laptop\vmangos\core\bin\RelWithDebInfo") {
-		$path = "D:\My files\svea_laptop\vmangos\core\bin\RelWithDebInfo"
 	} else {
 		$path = "~/vmangos/bin"
 	}
