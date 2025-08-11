199503cf2e4a5a27a1ba31a8f33c38073bb81bdc
Jonas Örnfelt <jonasornfelt@outlook.com>
2025-08-11 08:42:07 +0200

    update llama ps script

---
+++ my_scripts/llama_old.ps1
@@ -0,0 +1,66 @@
+# Usage:
+# .\llama.ps1
+# .\llama.ps1 cli
+# .\llama.ps1 server
+
+param(
+    [string]$mode = "server" # Default to "server" if no argument is provided
+)
+
+$basePath = Join-Path -Path $env:code_root_dir -ChildPath "Code\ml\llama.cpp\build\bin\Release"
+
+# Define potential model paths
+$modelPaths = @(
+    "C:/local/ai/models/bartowski_Llama-3.2-3B-Instruct-GGUF_Llama-3.2-3B-Instruct-Q4_K_M.gguf",
+    "C:/local/ai/models/ggml-org_gemma-3-1b-it-GGUF_gemma-3-1b-it-Q4_K_M.gguf",
+    "C:/local/ai/models/Meta-Llama-3.1-8B-Instruct-ggml-model-Q4_K_M.gguf",
+    "C:/local/ai/models/unsloth_DeepSeek-R1-0528-Qwen3-8B-GGUF_DeepSeek-R1-0528-Qwen3-8B-Q4_K_M.gguf",
+    "D:/my_files/my_docs/ai/models/bartowski_Llama-3.2-3B-Instruct-GGUF_Llama-3.2-3B-Instruct-Q4_K_M.gguf",
+    "D:/my_files/my_docs/ai/models/ggml-org_gemma-3-1b-it-GGUF_gemma-3-1b-it-Q4_K_M.gguf",
+    "D:/my_files/my_docs/ai/models/unsloth_DeepSeek-R1-0528-Qwen3-8B-GGUF_DeepSeek-R1-0528-Qwen3-8B-Q4_K_M.gguf",
+    "D:/my_files/my_docs/ai/models/Meta-Llama-3.1-8B-Instruct-ggml-model-Q4_K_M.gguf",
+    "../../../models/Meta-Llama-3.1-8B-Instruct/Meta-Llama-3.1-8B-Instruct-ggml-model-Q4_K_M.gguf",
+    "../../../models/meta-llama-3.1-8b-instruct-q4_k_m-gguf\meta-llama-3.1-8b-instruct-q4_k_m.gguf",
+    "../../../models/Meta-Llama-3.1-8B/Meta-Llama-3.1-8B-ggml-model-Q4_K_M.gguf",
+    "D:/2024/ollama/llama.cpp/models/Meta-Llama-3.1-8B/Meta-Llama-3.1-8B-ggml-model-Q4_K_M.gguf"
+)
+
+# Check for model existence case-insensitively
+function Get-ModelPath {
+    param (
+        [string[]]$paths
+    )
+    foreach ($path in $paths) {
+        $fullPath = Join-Path -Path $basePath -ChildPath $path
+        if (Test-Path -LiteralPath $fullPath -PathType Leaf) {
+            return $fullPath
+        }
+    }
+    return $null
+}
+
+# Get model path
+$modelPath = Get-ModelPath -paths $modelPaths
+
+if (-not $modelPath) {
+    Write-Error "Model file not found in any of the specified locations."
+    exit 1
+}
+Write-output "Model found at: $modelPath"
+
+# cd into dir
+Set-Location -Path $basePath
+
+# Run llama.cpp based on the given argument
+switch ($mode) {
+    "cli" {
+        .\llama-cli.exe -m $modelPath --threads $([System.Environment]::ProcessorCount) -c 2048 -cnv -p "you are a helpful assistant"
+    }
+    "server" {
+        .\llama-server.exe -m $modelPath --threads $([System.Environment]::ProcessorCount) -c 2048
+    }
+    default {
+        Write-Error "Invalid mode specified. Use 'cli' or 'server'."
+    }
+}
+
