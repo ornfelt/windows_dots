54cdddd25e5a7f5cc77585bdf583a68614a2d515
Jonas Örnfelt <jonasornfelt@outlook.com>
2025-08-11 16:50:14 +0200

    cmake script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -72,7 +72,8 @@ $aliases = @(
     ".stk", ".wow", ".wowbot", ".network_devices", ".network_devices_ping",
 	".mangos", ".llama", ".update_nvim_from_linux", ".docs", ".down", ".cdh", ".clean_shada",
     ".acore_update", ".tcore_update", ".gen_plant", ".gen_merm", ".git_push", ".git_pull",
-    ".cava", ".wc", ".list_mapped_drives", ".wow_wtf_update", ".wow_wtf_fix", ".mangos_update"
+    ".cava", ".wc", ".list_mapped_drives", ".wow_wtf_update", ".wow_wtf_fix", ".mangos_update",
+    ".cmake"
 )
 
 foreach ($alias in $aliases) {
