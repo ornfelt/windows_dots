8a4af8239b94489aba81356eaea1bd72ae515df3
Jonas Ornfelt <jonasornfelt@outlook.com>
2025-08-18 08:37:13 +0200

    use scale in gen_merm script

--- my_scripts/gen_merm.ps1
+++ my_scripts/gen_merm.ps1
@@ -29,11 +29,18 @@ if (-not $mermaidCli) {
 $outputFile = [System.IO.Path]::ChangeExtension($InputFile, ".png")
 $outputFileAlt = "$outputFile-1"
 
+#$useScale = $false
+$useScale = $true
+
 # npm install -g @mermaid-js/mermaid-cli
 # mmdc -i test.md -o test.png
-Write-Output "Running command: npx @mermaid-js/mermaid-cli@latest -i $InputFile -o $outputFile"
-
-npx @mermaid-js/mermaid-cli@latest -i "$InputFile" -o "$outputFile"
+if ($useScale) {
+    Write-Output "Running command: npx @mermaid-js/mermaid-cli@latest --scale 2 -i $InputFile -o $outputFile"
+    npx @mermaid-js/mermaid-cli@latest --scale 2 -i "$InputFile" -o "$outputFile"
+} else {
+    Write-Output "Running command: npx @mermaid-js/mermaid-cli@latest -i $InputFile -o $outputFile"
+    npx @mermaid-js/mermaid-cli@latest -i "$InputFile" -o "$outputFile"
+}
 
 if ((Test-Path $outputFile) -or (Test-Path $outputFileAlt)) {
     Write-Host "Mermaid diagram generated: $outputFile or $outputFileAlt" -ForegroundColor Green
