578a235dcdfb8be66086a9e1ee612ede81e6ee19
Jonas Ornfelt <jonasornfelt@outlook.com>
2025-09-24 12:09:07 +0200

    nvim: lsp powershell conf fixes for 0.11

--- nvim/lsp/powershell_es.lua
+++ nvim/lsp/powershell_es.lua
@@ -1,22 +1,28 @@
 if vim.fn.has('win32') == 1 then
-  local user_profile = vim.loop.os_getenv("USERPROFILE") or ""
-  local bundle_path = user_profile .. '/Downloads/PowerShellEditorServices'
-  if vim.fn.isdirectory(bundle_path) == 1 and vim.fn.executable('powershell.exe') == 1 then
+  local user_profile = vim.loop.os_getenv("USERPROFILE") or "C:/Users/jonas"
+  local base_dir = user_profile .. [[/Downloads/PowerShellEditorServices]]
+  local script_ps1 = base_dir .. [[/PowerShellEditorServices/Start-EditorServices.ps1]]
+  local log_path = user_profile .. [[/AppData/Local/nvim-data/powershell_es.log]]
+  local session_path = user_profile .. [[/AppData/Local/nvim-data/PowerShellEditorServices.json]]
+
+  if vim.fn.filereadable(script_ps1) == 1 and vim.fn.executable('powershell.exe') == 1 then
     return {
-      cmd = { 'powershell.exe', '-NoLogo', '-NoProfile', '-Command',
-        bundle_path .. [[\PowerShellEditorServices\Start-EditorServices.ps1]],
-        '-HostName', 'Neovim',
-        '-HostProfileId', 'Neovim',
-        '-HostVersion', '1.0.0',
-        '-LogLevel', 'Normal',
-        '-BundlePath', bundle_path,
-        '-Stdio'
+      cmd = {
+        'powershell.exe',
+        '-NoLogo',
+        '-NoProfile',
+        '-ExecutionPolicy', 'Bypass',
+        '-Command',
+        ( [[& '%s' -HostName 'Neovim' -HostProfileId 'Neovim' -HostVersion '1.0.0' ]] ..
+          [[-LogLevel 'Normal' -LogPath '%s' -SessionDetailsPath '%s' ]] ..
+          [[-BundledModulesPath '%s' -Stdio]] )
+          :format(script_ps1, log_path, session_path, base_dir),
       },
-      filetypes = { 'ps1', 'psm1', 'psd1' },
+      filetypes    = { 'ps1', 'psm1', 'psd1' },
       root_markers = { '.git' },
     }
   else
-    return {} -- no-op when not available
+    return {}
   end
 else
   return {}
