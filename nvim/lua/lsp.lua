fdf60d1549cf425cc1e9566bac9f63f5e78978cf
Jonas Örnfelt <jonasornfelt@outlook.com>
2025-10-10 07:49:56 +0200

    nvim: lsp native snippets fix

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -147,15 +147,12 @@ cmp.setup({
   -- If you actually use snippets (recommended), keep this enabled:
   snippet = {
     expand = function(args)
-      -- Use your preferred snippet engine:
-      -- vsnip:
-      vim.fn["vsnip#anonymous"](args.body)
-      -- luasnip:
-      -- require('luasnip').lsp_expand(args.body)
-      -- snippy:
-      -- require('snippy').expand_snippet(args.body)
-      -- ultisnips:
-      -- vim.fn["UltiSnips#Anon"](args.body)
+      -- Preferred snippet engine:
+      --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
     end,
   },
   window = {
