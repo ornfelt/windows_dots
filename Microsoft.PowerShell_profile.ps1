2d17b2191137662a514cd126ad6f3b52dadf88a5
Jonas Örnfelt <jonasornfelt@outlook.com>
2025-11-17 08:20:22 +0100

    ps: move some scripts

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -74,7 +74,7 @@ $aliases = @(
 	".mangos", ".llama", ".update_nvim_from_linux", ".docs", ".down", ".cdh", ".clean_shada",
     ".acore_update", ".tcore_update", ".gen_plant", ".gen_merm", ".git_push", ".git_pull",
     ".cava", ".wc", ".list_mapped_drives", ".wow_wtf_update", ".wow_wtf_fix", ".mangos_update",
-    ".cmake", ".cmangos_update", ".mw", ".mwc", ".mww"
+    ".cmake", ".cmangos_update", ".mw", ".mww", ".mwr"
 )
 
 foreach ($alias in $aliases) {
--- my_scripts/mw.ps1
+++ my_scripts/mw.ps1
@@ -1,8 +1,9 @@
-$scriptPath = "$env:code_root_dir/Code2/Wow/tools/my_wow/c++/tbc/run_with_args.ps1"
+$scriptPath = "$env:code_root_dir/Code2/Wow/tools/my_wow/cd_and_print.ps1"
 
 if (Test-Path $scriptPath) {
-    Write-Host "Running: & `$env:code_root_dir/Code2/Wow/tools/my_wow/c++/tbc/run_with_args.ps1 1" -ForegroundColor Cyan
-    & $scriptPath 1
+    Write-Host "Running: & `$env:code_root_dir/Code2/Wow/tools/my_wow/cd_and_print.ps1 $args" -ForegroundColor Cyan
+    # splat the args
+    & $scriptPath @args
 } else {
     Write-Host "Script not found: $scriptPath" -ForegroundColor Red
 }
