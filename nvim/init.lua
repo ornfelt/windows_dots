71e763b2e95c019e95b82b04ba61a536176e4de7
ornfelt <jonasornfelt@outlook.com>
2026-01-12 12:42:41 +0100

    nvim: cleanup lazy load of lsp and keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,28 +3,16 @@ require("lazy_plugins")
 require('options')
 
 -- lazy load these
---vim.api.nvim_create_autocmd("UIEnter", {
---  once = true,
---  callback = function()
---    vim.schedule(function()
---      --require('lsp_new')
---      --require('custom_statusline')
---      require('lsp')
---      require('keybindings')
---      require('autocmds')
---    end)
---  end,
---})
-
-
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
+vim.api.nvim_create_autocmd("UIEnter", {
+  once = true,
   callback = function()
+    vim.schedule(function()
       --require('lsp_new')
       --require('custom_statusline')
       require('lsp')
       require('keybindings')
       require('autocmds')
+    end)
   end,
 })
 
