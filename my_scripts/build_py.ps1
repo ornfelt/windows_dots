4ac7027c4418cf29144592275e37f4d26c02feee
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-04-23 07:47:55 +0200

    expand build helper script

---
+++ my_scripts/build_py.ps1
@@ -0,0 +1,8 @@
+$notes = $env:my_notes_path
+
+if ([string]::IsNullOrWhiteSpace($notes)) {
+    Write-Host "Environment variable 'my_notes_path' is not set."
+    exit 1
+}
+
+python "$notes/scripts/build.py"
