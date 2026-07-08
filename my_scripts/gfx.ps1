dd6c22bb4fb357ebf1e1595fcddfcbbc443f3cf9
Jonas Ornfelt <jonasornfelt@outlook.com>
2026-07-08 12:46:43 +0200

    new navigation scripts

---
+++ my_scripts/gfx.ps1
@@ -0,0 +1,6 @@
+# cd into gfx dir
+cd "$env:code_root_dir\Code2\General\gfx"
+# or:
+#cd (Join-Path $env:code_root_dir "Code2\General\gfx")
+# or:
+#Set-Location (Join-Path $env:code_root_dir "Code2\General\gfx")
