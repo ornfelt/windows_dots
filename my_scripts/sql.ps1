b90f7f28d6aadc459e91cedbcf81aa5729f7c828
ornfelt <jonasornfelt@outlook.com>
2026-09-03 11:25:58 +0200

    sql script

---
+++ my_scripts/sql.ps1
@@ -0,0 +1,6 @@
+# cd into my_sql dir
+cd "$env:code_root_dir\Code2\Sql\my_sql"
+# or:
+#cd (Join-Path $env:code_root_dir "Code2\Sql\my_sql")
+# or:
+#Set-Location (Join-Path $env:code_root_dir "Code2\Sql\my_sql")
